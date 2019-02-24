0cbf4e6d593797c983abc77cb57a4acec8461bc4
Hayden <haydendrussell@gmail.com>
2019-02-24 00:38:56 -0500

    [NEW] Adding listeners for drawer
    -Note this drawer element/functionality will most likely be separated out into its own file later

--- app/src/main/java/com/hdrussell/apps/steam/main/MainController.kt
+++ app/src/main/java/com/hdrussell/apps/steam/main/MainController.kt
@@ -7,7 +7,9 @@ import android.support.v4.widget.DrawerLayout
 import android.support.v7.app.ActionBar
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.Toolbar
+import android.util.Log
 import android.view.MenuItem
+import android.view.View
 import com.hdrussell.apps.R
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -28,6 +30,26 @@ class MainController : AppCompatActivity() {
 
             true
         }
+
+        drawerLayout.addDrawerListener(
+                object : DrawerLayout.DrawerListener {
+                    override fun onDrawerSlide(p0: View, p1: Float) {
+                        Log.d("drawerElement", "drawerSlide")
+                    }
+
+                    override fun onDrawerOpened(p0: View) {
+                        Log.d("drawerElement", "drawerOpened")
+                    }
+
+                    override fun onDrawerClosed(p0: View) {
+                        Log.d("drawerElement", "drawerClosed")
+                    }
+
+                    override fun onDrawerStateChanged(p0: Int) {
+                        Log.d("drawerElement", "drawerChanged")
+                    }
+                }
+        )
 
         val toolbar: Toolbar = findViewById(R.id.toolbar)
         setSupportActionBar(toolbar)
