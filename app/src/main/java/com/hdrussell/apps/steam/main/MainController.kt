45716f92fab180c4cd03486f1f37eaa05c57fb40
Hayden <haydendrussell@gmail.com>
2019-02-23 23:51:42 -0500

    [NEW] Adding prelim navigation
    -Note, all values are currently placeholders

--- app/src/main/java/com/hdrussell/apps/steam/main/MainController.kt
+++ app/src/main/java/com/hdrussell/apps/steam/main/MainController.kt
@@ -2,11 +2,13 @@ package com.hdrussell.apps.steam.main
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import com.hdrussell.apps.R
 
 class MainController : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
     }
 
 }
