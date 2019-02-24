98055a6449da024f77fdd80eb5dc770e433d3288
Hayden <haydendrussell@gmail.com>
2019-02-24 00:24:14 -0500

    [NEW] Adding more to prelim Nav

--- app/src/main/java/com/hdrussell/apps/steam/main/MainController.kt
+++ app/src/main/java/com/hdrussell/apps/steam/main/MainController.kt
@@ -1,14 +1,37 @@
 package com.hdrussell.apps.steam.main
 
 import android.os.Bundle
+import android.support.v4.view.GravityCompat
+import android.support.v7.app.ActionBar
 import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.Toolbar
+import android.view.MenuItem
 import com.hdrussell.apps.R
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainController : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val toolbar: Toolbar = findViewById(R.id.toolbar)
+        setSupportActionBar(toolbar)
+        val actionbar: ActionBar? = supportActionBar
+        actionbar?.apply {
+            setDisplayHomeAsUpEnabled(true)
+            setHomeAsUpIndicator(R.drawable.ic_menu_black_24dp)
+        }
+    }
+
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        return when (item?.itemId) {
+            android.R.id.home -> {
+                drawer_layout.openDrawer(GravityCompat.START)
+                true
+            } else -> super.onOptionsItemSelected(item)
+        }
     }
 
 }
