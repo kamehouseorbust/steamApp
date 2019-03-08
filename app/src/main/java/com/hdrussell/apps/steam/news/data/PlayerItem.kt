66082b79bce5e7554f08bcc8c620a413c01a66a5
Hayden <haydendrussell@gmail.com>
2019-03-07 20:01:00 -0500

    [NEW] Creating Player Items for Profile Data

---
+++ app/src/main/java/com/hdrussell/apps/steam/news/data/PlayerItem.kt
@@ -0,0 +1,21 @@
+package com.hdrussell.apps.steam.news.data
+
+data class PlayerItem (
+        val steamid: String,
+        val communityvisibilitystate: Int,
+        val profilestate: Int,
+        val personaname: String,
+        val lastlogoff: Double,
+        val profileurl: String,
+        val avatar: String,
+        val avatarmedium: String,
+        val avatarfull: String,
+        val personastate: Int,
+        val realname: String,
+        val primaryclanid: String,
+        val timecreated: Double,
+        val personastateflags: Int,
+        val loccountrycode: String,
+        val locstatecode: String,
+        val loccityid: Int
+)
---
+++ app/src/main/java/com/hdrussell/apps/steam/news/data/PlayerItems.kt
@@ -0,0 +1,5 @@
+package com.hdrussell.apps.steam.news.data
+
+data class PlayerItems (
+        val playerItemsList: ArrayList<PlayerItem>
+)
